94955232cee3ead8e6705bac318a476a587ec676
David Stainton <dstainton415@gmail.com>
2016-11-30 05:06:07 +0000

    Add cbor encoding handler methods for sphinx packets

--- txmix/cbor_encoding_handler.py
+++ txmix/cbor_encoding_handler.py
@@ -13,3 +13,18 @@ class CBOREncodingHandler():
 
     def deserialize(self, message):
         return cbor.loads(message)
+
+    def serialize_sphinx_packet(self, alpha=None, beta=None, gamma=None, delta=None):
+        sphinx_packet = {
+            "alpha": alpha,
+            "beta" : beta,
+            "gamma": gamma,
+            "delta": delta,
+        }
+        return self.serialize(sphinx_packet)
+
+    def deserialize_sphinx_packet(self, message):
+        sphinx_packet = self.deserialize(message)
+        for key in ["alpha", "beta", "gamma", "delta"]:
+            assert key in sphinx_packet
+        return sphinx_packet
