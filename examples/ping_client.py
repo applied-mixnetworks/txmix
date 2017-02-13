041ffd8c4f9d779a2f3a3f67b7421d1df1133ff8
David Stainton <dstainton415@gmail.com>
2017-02-13 01:34:08 +0000

    Add work in progress onion transport ping client

--- examples/ping_client.py
+++ examples/ping_client.py
@@ -1,34 +1,81 @@
 
-from twisted.internet import reactor
 
-from sphinxmixcrypto import SphinxParams
-from txmix import UDPTransport
-from txmix import MixClient, RandomRouteFactory
+import attr
+import click
+from twisted.internet import reactor, defer
+
+from sphinxmixcrypto import SphinxParams, IMixPKI, IReader
+from txmix import OnionTransportFactory
+from txmix import MixClient, RandomRouteFactory, IMixTransport, IRouteFactory
 from txmix import DummyPKI, RandReader
 
 
-def main():
-    params = SphinxParams(5, 1024)
+@attr.s
+class PingClient(object):
+
+    params = attr.ib(validator=attr.validators.instance_of(SphinxParams))
+    pki = attr.ib(validator=attr.validators.provides(IMixPKI))
+    client_id = attr.ib(validator=attr.validators.instance_of(bytes))
+    rand_reader = attr.ib(validator=attr.validators.provides(IReader))
+    transport = attr.ib(validator=attr.validators.provides(IMixTransport))
+    route_factory = attr.ib(validator=attr.validators.provides(IRouteFactory))
+
+    reply = ""
+    reply_d = None
+
+    def message_received(self, message):
+        print "DEBUG: PingClient: message_received: %s" % message
+        self.reply = message
+        self.reply_d.callback(message)
+
+    def start(self):
+        self.client = MixClient(self.params,
+                                self.pki,
+                                self.client_id,
+                                self.rand_reader,
+                                self.transport,
+                                self.message_received,
+                                self.route_factory)
+        d = self.client.start()
+        return d
+
+    def wait_for_reply(self):
+        return self.reply_d
+
+    def send(self, destination, message):
+        """
+        proxy to the MixClient's send method.
+        returns a deferred
+        """
+        return self.client.send(destination, message)
+
+
+@click.command()
+@click.option('--tor-control-unix-socket', default=None, type=str, help="unix socket name for connecting to the tor control port")
+@click.option('--tor-control-tcp-host', default=None, type=str, help="tcp host for connecting to the tor control port")
+@click.option('--tor-data', default=None, type=str, help="launch tor data directory")
+@defer.inlineCallbacks
+def main(tor_control_unix_socket, tor_control_tcp_host, tor_data):
+    params = SphinxParams(max_hops=5, payload_size=1024)
     pki = DummyPKI()
     client_id = b"client"
     rand_reader = RandReader()
-    transport = UDPTransport(reactor, ("127.0.0.1", 6779))
+    transport_factory = OnionTransportFactory(reactor, params, tor_control_unix_socket)
+    transport = transport_factory.buildTransport()
     route_factory = RandomRouteFactory(params, pki, rand_reader)
-
-    def message_receive_handler(message):
-        print "client received message: %s" % message
-
-    client = MixClient(params, pki, client_id, rand_reader, transport, message_receive_handler, route_factory)
-    d = client.start()
-
-    def use_client(result):
-        message = b"ping"
-        destination = pki.identities()[0]
-        client.send(destination, message)
-
-    d.addCallback(use_client)
-    reactor.run()
+    destination = pki.identities()[0]  # XXX todo: pick a more interesting destination
+    message = b"ping"
+    client = PingClient(params, pki, client_id, rand_reader, transport, route_factory)
+    yield client.start()
+    yield client.send(destination, message)
+    yield client.wait_for_reply()
+    defer.returnValue(None)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        reactor.callWhenRunning(main)
+    except ClickException, e:
+        e.show()
+        sys.exit(1)
+    reactor.run()
