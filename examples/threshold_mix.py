db8b6d28b41388d29996b7781dcee8453d7f8825
David Stainton <dstainton415@gmail.com>
2017-02-13 21:47:43 +0000

    Add crunch-bang and fix cli for client and mix

--- examples/threshold_mix.py
+++ examples/threshold_mix.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 
 import click
 from twisted.internet import reactor
@@ -10,12 +10,12 @@ from txmix import RandReader, generate_node_keypair, generate_node_id, DummyPKI,
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('--tor-control-unix-socket', default=None, type=str, help="unix socket name for connecting to the tor control port")
-@click.option('--tor-control-tcp-host', default=None, type=str, help="tcp host for connecting to the tor control port")
-@click.option('--tor-control-tcp-port', default=None, type=str, help="tcp port for connecting to the tor control port")
-@click.option('--onion-unix-socket', default=None, type=str, help="unix socket file that our onion service should use")
-@click.option('--onion-tcp-interface-ip', default=None, type=str, help="the interface to listen on for onion connections")
-@click.option('--tor-data', default=None, type=str, help="launch tor data directory")
+@click.option('--tor-control-unix-socket', default="", type=str, help="unix socket name for connecting to the tor control port")
+@click.option('--tor-control-tcp-host', default="", type=str, help="tcp host for connecting to the tor control port")
+@click.option('--tor-control-tcp-port', default=0, type=int, help="tcp port for connecting to the tor control port")
+@click.option('--onion-unix-socket', default="", type=str, help="unix socket file that our onion service should use")
+@click.option('--onion-tcp-interface-ip', default="", type=str, help="the interface to listen on for onion connections")
+@click.option('--tor-data', default="", type=str, help="launch tor data directory")
 def main(tor_control_unix_socket,
          tor_control_tcp_host,
          tor_control_tcp_port,
@@ -32,16 +32,16 @@ def main(tor_control_unix_socket,
     threshold_count = 100
     transport_factory = OnionTransportFactory(reactor,
                                               params,
-                                              tor_control_unix_socket,
-                                              tor_control_tcp_host,
+                                              tor_control_unix_socket.encode('utf-8'),
+                                              tor_control_tcp_host.encode('utf-8'),
                                               tor_control_tcp_port,
-                                              onion_unix_socket,
-                                              onion_tcp_interface_ip)
-    d = transport_factory.buildTransport()
+                                              onion_unix_socket.encode('utf-8'),
+                                              onion_tcp_interface_ip.encode('utf-8'))
+    d = transport_factory.build_transport()
 
     def got_transport(transport):
         mix = ThresholdMixNode(threshold_count, node_id, replay_cache, key_state, params, pki, transport)
-        mix.start()
+        return mix.start()
 
     d.addCallback(got_transport)
     reactor.run()
